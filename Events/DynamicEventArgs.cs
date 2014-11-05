aeadf3361fe8b281b242f37390a4a57f829d2ab5
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-11-05 02:45:40 +0100

    Added a Dynamic EventArgs

---
+++ Events/DynamicEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace Utils.Events
+{
+    public class DynamicEventArgs : EventArgs
+    {
+        public readonly dynamic Args = new ExpandoObject();
+
+        public DynamicEventArgs()
+            : base()
+        { }
+
+    }
+}
