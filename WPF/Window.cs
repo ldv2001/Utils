88840939cb84d709ac17b3de0aa38889d29db64c
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-11-05 01:21:23 +0100

    Added a method to center a WPF window on screen

---
+++ WPF/Window.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Utils.WPF
+{
+    public static class Window
+    {
+
+        /// <summary>
+        /// Centers the window.
+        /// </summary>
+        /// @author : Wild_A (http://stackoverflow.com/users/621044/wild-a)
+        /// @original-post : http://stackoverflow.com/a/14334262/2245256
+        /// <param name="window">The window.</param>
+        public static void CenterWindow(System.Windows.Window window)
+        {
+            Rect workArea = System.Windows.SystemParameters.WorkArea;
+            window.Left = (workArea.Width - window.Width) / 2 + workArea.Left;
+            window.Top = (workArea.Height - window.Height) / 2 + workArea.Top;
+        }
+    }
+}
