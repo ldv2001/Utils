87ce9f80e3485251042c6df9abf2a09b557697c8
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-01-29 10:52:41 -0500

    Added Methods Left/Right for tuples in Utils.System.TupleExtensions

---
+++ System/TupleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utils.System
+{
+    public static class TupleExtensions
+    {
+
+        public static T Left<T, T1>(this Tuple<T, T1> t)
+        {
+            return t.Item1;
+        }
+
+        public static T1 Right<T, T1>(this Tuple<T, T1> t)
+        {
+            return t.Item2;
+        }
+    }
+}
--- Text/StringOperations.cs
+++ Text/StringOperations.cs
@@ -24,14 +24,24 @@ namespace Utils.Text
         /// <summary>
         /// Allows to use <see cref="Regex.Replace(string,string,string,System.Text.RegularExpressions.RegexOptions)"/> directly on a String
         /// </summary>
-        /// <param name="input"></param>
-        /// <param name="pattern"></param>
-        /// <param name="replacement"></param>
-        /// <param name="options"></param>
+        /// <param name="input">The string to compute</param>
+        /// <param name="pattern">The Regexp to apply</param>
+        /// <param name="replacement">The replacement expression</param>
+        /// <param name="options">Regular Expression engine options</param>
         /// <returns></returns>
         public static String RegExpReplace(this String input, String pattern, String replacement, RegexOptions options = RegexOptions.None)
         {
             return Regex.Replace(input, pattern, replacement, options);
+        }
+
+        /// <summary>
+        /// Check if a string is Empty
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>True if empty, false otherwise</returns>
+        public static Boolean IsEmpty(this String str)
+        {
+            return str.Equals(String.Empty);
         }
     }
 }
