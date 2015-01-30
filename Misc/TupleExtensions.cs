d515c2991ee98e6e1d1874b63eeef89dfb686393
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-01-30 15:15:43 -0500

    Changed namespace for TupleExtensions (from Utils.System to Utils.Misc) to avoid name collisions

--- Misc/TupleExtensions.cs
+++ Misc/TupleExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Utils.System
+namespace Utils.Misc
 {
     public static class TupleExtensions
     {
--- WPF/WindowExtensions.cs
+++ WPF/WindowExtensions.cs
@@ -16,9 +16,9 @@ namespace Utils.WPF
         /// <param name="window">The window.</param>
         /// @author : Wild_A (http://stackoverflow.com/users/621044/wild-a)
         /// @original-post : http://stackoverflow.com/a/14334262/2245256
-        public static void CenterWindow(this System.Windows.Window window)
+        public static void CenterWindow(this Window window)
         {
-            Rect workArea = System.Windows.SystemParameters.WorkArea;
+            Rect workArea = SystemParameters.WorkArea;
             window.Left = (workArea.Width - window.Width) / 2 + workArea.Left;
             window.Top = (workArea.Height - window.Height) / 2 + workArea.Top;
         }
