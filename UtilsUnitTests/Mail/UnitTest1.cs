1a082830c975c497b7b3498681eb6384edf03275
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-03-24 11:23:57 -0400

    Created SubProject

---
+++ UtilsUnitTests/Mail/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilsUnitTests.Mail
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UtilsUnitTests/Time/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilsUnitTests.Time
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
