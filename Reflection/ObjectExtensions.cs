f87ee6dbc1c8160f9f066753b6ef1bbc82d0d0d6
Antoine-Ali Zarrouk <antoine-ali.zarrouk@soprasteria.com>
2016-05-04 10:26:18 +0200

    Added GetPropertyInfo Method

---
+++ Reflection/ObjectExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Utils.Reflection
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Méthode permettant d'obtenir les informations d'une propriété de classe
+        /// </summary>
+        /// <remarks>
+        /// Created by : Cameron MacFarland @ http://stackoverflow.com/a/672212/2245256
+        /// </remarks>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="propertyLambda"></param>
+        /// <returns></returns>
+        public static PropertyInfo GetPropertyInfo<TSource, TProperty>(
+            this TSource source,
+            Expression<Func<TSource, TProperty>> propertyLambda)
+        {
+            Type type = typeof(TSource);
+
+            MemberExpression member = propertyLambda.Body as MemberExpression;
+            if (member == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a method, not a property.",
+                    propertyLambda));
+
+            PropertyInfo propInfo = member.Member as PropertyInfo;
+            if (propInfo == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a field, not a property.",
+                    propertyLambda));
+
+            if (type != propInfo.ReflectedType &&
+                !type.IsAssignableFrom(propInfo.ReflectedType))
+                throw new ArgumentException(string.Format(
+                    "Expresion '{0}' refers to a property that is not from type {1}.",
+                    propertyLambda,
+                    type));
+
+            return propInfo;
+        }
+    }
+}
