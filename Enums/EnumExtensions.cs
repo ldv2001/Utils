c7338b5d6d461ab8163993daaef7d6285dfee613
Antoine-Ali Zarrouk <antoine-ali.zarrouk@soprasteria.com>
2016-11-24 13:56:31 +0100

    Generic version of the GetDescription method

--- Enums/EnumExtensions.cs
+++ Enums/EnumExtensions.cs
@@ -39,5 +39,39 @@ namespace Utils.Enums
             }
             return value.ToString();
         }
+
+        /// <summary>
+        /// Method used to get a User-Friendly description for an enum member
+        /// </summary>
+        /// <typeparam name="T">The Enum Type</typeparam>
+        /// <param name="enumerationValue">The enum value to get the description for</param>
+        /// <returns>The content of the <see cref="DescriptionAttribute"/> if one is found on the num member, if not, returns <see cref="Enum.ToString()"/></returns>
+        /// <remarks>
+        /// From : http://stackoverflow.com/a/479417/2245256
+        /// </remarks>
+        public static string GetDescription<T>(this T enumerationValue) where T : struct
+        {
+            Type type = enumerationValue.GetType();
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException("EnumerationValue must be of Enum type", "enumerationValue");
+            }
+
+            //Tries to find a DescriptionAttribute for a potential friendly name
+            //for the enum
+            MemberInfo[] memberInfo = type.GetMember(enumerationValue.ToString());
+            if (memberInfo != null && memberInfo.Length > 0)
+            {
+                object[] attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                {
+                    //Pull out the description value
+                    return ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+            //If we have no description attribute, just return the ToString of the enum
+            return enumerationValue.ToString();
+        }
     }
 }
